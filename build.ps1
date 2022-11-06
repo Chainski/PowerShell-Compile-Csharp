3b2ab7bc14e3d4bb157a9768bf8a885701fc1387
Chainski <96607632+Chainski@users.noreply.github.com>
2022-11-06 14:51:43 -0600

    Create build.ps1

---
+++ build.ps1
@@ -0,0 +1,38 @@
+# https://github.com/Chainski/PowerShell-Compile-Csharp
+# Simple Hello World Program in C# :)
+
+
+$source  = @" 
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+ 
+namespace Chainski
+{
+    class Program
+    {
+        static void Main()
+        {
+           MessageBox.Show("Hello, World !");
+        }
+    }
+}
+"@
+$outpath  =  "$env:userprofile\downloads\hello.exe" 
+$main  =  "Hello"  
+$dict = new-object 'System.Collections.Generic.Dictionary[string,string]' 
+$dict.Add('CompilerVersion','v4.0')
+$CsharpCompiler = New-Object Microsoft.CSharp.CSharpCodeProvider $dict
+$CompilerParametres = New-Object System.CodeDom.Compiler.CompilerParameters
+$CompilerParametres.ReferencedAssemblies.Add("System.dll")
+$CompilerParametres.ReferencedAssemblies.Add("System.Windows.Forms.dll")
+$CompilerParametres.IncludeDebugInformation = $false
+$CompilerParametres.GenerateExecutable = $true
+$CompilerParametres.GenerateInMemory = $false
+$CompilerParametres.OutputAssembly  =  $outpath 
+$CompilerParametres.CompilerOptions += "/platform:X86 /unsafe /target:winexe"
+$result = $CsharpCompiler.CompileAssemblyFromSource($CompilerParametres, $source) 
+$result  
+ 
